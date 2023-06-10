7f1dfd54c29b1439804034962cde788598cf132c
Henry <105891749+HenryIto@users.noreply.github.com>
2023-06-09 22:00:27 -0300

    Update Sprite

--- src/characters/Entity.java
+++ src/characters/Entity.java
@@ -1,6 +1,13 @@
 package characters;
 
+import java.awt.image.BufferedImage;
+
 public class Entity {
-	int X, Y;
-	float Speed = 0f;
+	public int X, Y;
+	public float Speed = 0f;
+	
+	// BufferedImage descreve uma imagem com um buffer acessível de dados de imagem
+	// nos usamos isso para armazenar os nossos arquivos de imagem
+	public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
+	public String direction;
 }
--- src/characters/Player.java
+++ src/characters/Player.java
@@ -2,6 +2,9 @@ package characters;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
 
 import main.GamePanel;
 import main.KeyHandler;
@@ -17,6 +20,39 @@ public class Player extends Entity {
 	public Player(GamePanel gamePanel, KeyHandler keyHandler) {
 		this.gp = gamePanel;
 		this.key = keyHandler;
+		
+		setDefaultValues();
+		getPlayerImage();
+	}
+	
+	public void setDefaultValues() {
+		X = 100;
+		Y = 100;
+		Speed = 4;
+		direction = "down";
+	}
+	
+	public void getPlayerImage() {
+		
+		try {
+			
+			// ImageIO.read carrega uma imagem que esta dentro do arquivo
+			// getClass().getResourceAsStream("/Player/boy_up_1.png") retorna um objeto 'InputStream' que representa o fluxo de bytes da imagem 'boy_up_1.png'
+			// getClass() retorna o objeto Class para a classe em que esse código está sendo executado
+			// getResourceAsStream() é um método dessa classe que permite obter um fluxo de bytes para um recurso no mesmo diretório ou pacote da classe.
+			// ImageIO.read() lê o fluxo de bytes da imagem e retorna um objeto BufferedImage que representa a imagem carregada. 
+			// Portanto, essa linha de código carrega a imagem "boy_up_1.png" e retorna um objeto BufferedImage contendo essa imagem.
+			up1 = ImageIO.read(getClass().getResourceAsStream("../assets/sprites/Player/boy_up_1.png"));
+			up2 = ImageIO.read(getClass().getResourceAsStream("../assets/sprites/Player/boy_up_2.png"));
+			down1 = ImageIO.read(getClass().getResourceAsStream("../assets/sprites/Player/boy_down_1.png"));
+			down2 = ImageIO.read(getClass().getResourceAsStream("../assets/sprites/Player/boy_down_2.png"));
+			left1 = ImageIO.read(getClass().getResourceAsStream("../assets/sprites/Player/boy_left_1.png"));
+			left2 = ImageIO.read(getClass().getResourceAsStream("../assets/sprites/Player/boy_left_2.png"));
+			right1 = ImageIO.read(getClass().getResourceAsStream("../assets/sprites/Player/boy_right_1.png"));
+			right2 = ImageIO.read(getClass().getResourceAsStream("../assets/sprites/Player/boy_right_2.png"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void Update() {
@@ -29,10 +65,14 @@ public class Player extends Entity {
 		}
 		
 		if (verticalMovement) {
+			String verticalImageDirection = key.downPressed ? "down" : "up";
+			direction = verticalImageDirection;
 			int verticalDirection = key.downPressed ? 1 : -1; // isso vai fazer o Player ir para cima ou para baixo dependendo de qual tecla estiver apertada
 			Y += Speed * verticalDirection; // isso vai fazer o Player se mover na _vertical_
 		}
 		if (horizontalMovement) {
+			String horizontalImageDirection = key.rightPressed ? "right" : "left";
+			direction = horizontalImageDirection;
 			int horizontalDirection = key.rightPressed ? 1 : -1; // isso vai fazer o Player ir para esquerda ou para direita dependendo de qual tecla estiver apertada
 			X += Speed * horizontalDirection; // isso vai fazer o Player se mover na _horizontal_
 		}
@@ -41,8 +81,28 @@ public class Player extends Entity {
 	
 	// este é o sprite do player
 	public void Draw(Graphics2D g2) {
-		g2.setColor(Color.white);
-		// desenha um quadrado e preeche com a cor branca (int x, int y, int widht, int height)
-		g2.fillRect(X, Y, gp.tileSize, gp.tileSize);
+//		g2.setColor(Color.white);
+//		// desenha um quadrado e preeche com a cor branca (int x, int y, int widht, int height)
+//		g2.fillRect(X, Y, gp.tileSize, gp.tileSize);
+		
+		BufferedImage image = null;
+		
+		switch(direction) {
+		case "up":
+			image = up1;
+		break;
+		case "down":
+			image = down1;
+		break;
+		case "left":
+			image = left1;
+		break;
+		case "right":
+			image = right1;
+		break;
+		}
+		
+		// desenha a imagem na tela
+		g2.drawImage(image, X, Y, gp.tileSize, gp.tileSize, null);
 	}
 }
