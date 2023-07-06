0030f2c60b4bdc750e9d2b775847c75426599bee
Ever <evertonxdoj@gmail.com>
2023-07-06 09:01:01 -0300

    game renamed

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,7 +10,7 @@ public class Main {
 		JFrame window = new JFrame();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setResizable(false);
-		window.setTitle("Jogo dos crias");
+		window.setTitle("Shadows of Eternity");
 		
 		GamePanel gamePanel = new GamePanel();
 		window.add(gamePanel);
